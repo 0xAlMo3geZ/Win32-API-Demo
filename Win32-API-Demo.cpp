14bbca50da5b0a7bd9e724d5d00c54e35546fd1d
Muhammad Zaki <mahmadzekoo@gmail.com>
2023-12-17 23:42:03 +0200

    Entry Point mod and first window

--- Win32-API-Demo.cpp
+++ Win32-API-Demo.cpp
@@ -1,3 +1,8 @@
 #include <windows.h>
 
-int 
+int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
+{
+    MessageBoxW(NULL, L"Hello", L"My First GUI", MB_OK);
+
+    return 0;
+}
