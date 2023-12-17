67f694b3f6bb82266e201629dd1e644d363802e5
Muhammad Zaki <mahmadzekoo@gmail.com>
2023-12-18 01:26:54 +0200

    done creating the window

--- Win32-API-Demo.cpp
+++ Win32-API-Demo.cpp
@@ -1,8 +1,39 @@
 #include <windows.h>
 
-int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
+LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
 {
-    MessageBoxW(NULL, L"Hello", L"My First GUI", MB_OK);
+    switch (msg)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProcW(hWnd, msg, wp, lp);
+    }
+}
+
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    WNDCLASS wc = { 0 };
+
+    wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hInstance = hInstance;
+    wc.lpszClassName = L"myWindowClass";
+    wc.lpfnWndProc = WindowProcedure;
+
+    if (!RegisterClass(&wc))
+        return -1;
+    
+    CreateWindowW(L"myWindowClass", L"My Window", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 100, 100, 500, 500, NULL, NULL, NULL, NULL);
+
+    MSG msg = { 0 };
+
+    while (GetMessage(&msg, NULL, NULL, NULL))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
 
     return 0;
 }
